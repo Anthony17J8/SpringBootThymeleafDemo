b964161582356c182601cdbbfd57316806e449e3
Kiselev <j8kiselev@gmail.com>
2019-02-10 12:50:21 +0300

    Update employee

--- src/main/java/ru/icoltd/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/ru/icoltd/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package ru.icoltd.springboot.thymeleafdemo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.icoltd.springboot.thymeleafdemo.entity.Employee;
 import ru.icoltd.springboot.thymeleafdemo.service.EmployeeService;
 
@@ -54,5 +51,18 @@ public class EmployeeController {
 
         // use a redirect to prevent duplicate submissions
         return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+
+        // get the employee from db
+        Employee theEmployee = employeeService.findById(theId);
+
+        // set employee as a model attribute to pre-populate the form
+        theModel.addAttribute("employee", theEmployee);
+
+        // send over to our form
+        return "employees/employee-form";
     }
 }
