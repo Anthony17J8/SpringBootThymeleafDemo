2ef8ffcb4214e5048d5469dec6704daebc847c81
Kiselev <j8kiselev@gmail.com>
2019-02-10 12:16:16 +0300

    Sort employees by last name

--- src/main/java/ru/icoltd/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/ru/icoltd/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,5 +3,10 @@ package ru.icoltd.springboot.thymeleafdemo.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.icoltd.springboot.thymeleafdemo.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // add a method to sort by last name
+    List<Employee> findAllByOrderByLastNameAsc();
 }
